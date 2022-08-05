1300c2f038f9b0c369d719108d7e59e6f430caab
Iniobong Benson <linibensonjr@gmail.com>
2022-08-05 15:23:13 +0100

    feat: Add Create Batch Thiessen Polygon

--- ThiessenPolygons/BatchCreateThiessenPolygons.py
+++ ThiessenPolygons/BatchCreateThiessenPolygons.py
@@ -1,41 +1,52 @@
-import arcpy, os
+import arcpy
+import os
+
+
 def ScriptTool(param0, param1, param2, param3):
     # Script execution code goes here
 
     arcpy.env.workspace = param0
-    #Extract the names of the feature dataset
-    units = arcpy.Describe(param1).name
-    arcpy.AddMessage(units)
-    lgas = arcpy.Describe(param2).name
-    arcpy.AddMessage(lgas)
-    # ds = arcpy.ListDatasets()
-    
+
+    # Extract the names of the feature dataset
+    points_features = arcpy.Describe(param1).name
+    arcpy.AddMessage(points_features)
+    processing_extents = arcpy.Describe(param2).name
+    arcpy.AddMessage(processing_extents)
+
     outpath = param3
-    unit_list = arcpy.ListFeatureClasses(feature_dataset=units)
-    unit_list.sort()
-    lga_list = arcpy.ListFeatureClasses(feature_dataset=lgas)
-    lga_list.sort()
-    if len(unit_list) != len(lga_list):
-        arcpy.AddError('Some LGAs or Polling units are missing')
+    #Get feature classes from feature datasets and sort them alphabetically
+    points_list = arcpy.ListFeatureClasses(feature_dataset=points_features)
+    points_list.sort()
+    extents_list = arcpy.ListFeatureClasses(feature_dataset=processing_extents)
+    extents_list.sort()
+
+    #Confirm that there are equal number of points and processing extent
+    if len(points_list) != len(extents_list):
+        arcpy.AddError('Some processing_extents or Polling points_features are missing')
     else:
-    
-        for fc in unit_list:
+        for fc in points_list:
             temp_thiessen = r"memory\\outpt"
-            clip_out = os.path.join(arcpy.env.workspace, fc)+'Units'
-            arcpy.AddMessage(clip_out)
-            arcpy.env.extent = lga_list[unit_list.index(fc)]
+            clip_out = os.path.join(arcpy.env.workspace, fc+"Poly")
+
+            #Get the extents of the extent feature class
+            arcpy.env.extent = extents_list[points_list.index(fc)]
             xmin, ymin, xmax, ymax = arcpy.env.extent.XMin, arcpy.env.extent.YMin, arcpy.env.extent.XMax, arcpy.env.extent.YMax
             with arcpy.EnvManager(extent='{} {} {} {}'.format(xmin, ymin, xmax, ymax)):
                 arcpy.analysis.CreateThiessenPolygons(fc, temp_thiessen, 'ALL')
-            arcpy.analysis.Clip(temp_thiessen, lga_list[unit_list.index(fc)] ,clip_out)
-        
-        arcpy.AddMessage(unit_list)
-        arcpy.Delete_management(temp_thiessen)
-        return
+                clipped = arcpy.analysis.Clip(
+                temp_thiessen, extents_list[points_list.index(fc)], clip_out)
+
+
+    arcpy.AddMessage(points_list)
+    arcpy.Delete_management(temp_thiessen)
+    return
+
 
 def getUnits(dataset):
     arcpy.env.workspace = dataset
     return arcpy.ListFeatureClasses()
+
+
 def getLGAs(dataset):
     arcpy.env.workspace = dataset
     return arcpy.ListFeatureClasses()
@@ -48,9 +59,9 @@ if __name__ == '__main__':
     param1 = arcpy.GetParameterAsText(1)
     param2 = arcpy.GetParameterAsText(2)
     param3 = arcpy.GetParameterAsText(3)
-    
+
     ScriptTool(param0, param1, param2, param3)
     getUnits(param1)
     getLGAs(param2)
-    
+
     # Update derived parameter values using arcpy.SetParameter() or arcpy.SetParameterAsText()
